aae8293fb1dbe24cf1e5d6678588875dfc7a1908
LAPTOP-UQO9PN3K\rames <rameswarareddy316@gmail.com>
2023-06-29 20:30:03 +0530

    latest change

---
+++ main/searchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function searchBar() {
+  return (
+    <div>
+      <ul>
+        <li>ram</li>
+        <li>reddy</li>
+        <li>gangireddy</li>
+      </ul>
+    </div>
+  )
+}
+
+export default searchBar
